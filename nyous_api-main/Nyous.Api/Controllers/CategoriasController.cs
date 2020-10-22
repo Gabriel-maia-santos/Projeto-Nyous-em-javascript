11d251a32baec6b6582bbc5e9246e11f8d0459fb
Gabriel-maia-santos <gabis.maia.santos@gmail.com>
2020-10-22 01:22:35 -0300

    First commit

---
+++ nyous_api-main/Nyous.Api/Controllers/CategoriasController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Nyous.Api.Domains;
+using Nyous.Api.DTO;
+using Nyous.Api.Interfaces.Repositorios;
+
+namespace Nyous.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriasController : ControllerBase
+    {
+        private readonly ICategoriaRepositorio _categoriaRepositorio;
+
+        public CategoriasController(ICategoriaRepositorio categoriaRepositorio)
+        {
+            _categoriaRepositorio = categoriaRepositorio;
+        }
+
+        [HttpPost]
+        //[Authorize(Roles = "Admin" )]
+        public IActionResult Cadastrar(CategoriaDTO categoria)
+        {
+            try
+            {
+                Categoria cat = new Categoria();
+                cat.Nome = categoria.Nome;
+                cat.UrlImagem = categoria.UrlImagem;
+
+                _categoriaRepositorio.Adicionar(cat);
+
+                return Ok(cat);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Listar()
+        {
+            try
+            {
+                var categorias = _categoriaRepositorio.ObterTodos();
+
+                return Ok(categorias);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
